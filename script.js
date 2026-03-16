9ac653ca4f928729d9873c11374375410fb33f25
Tim Meyer <timmeyer11-alt@users.noreply.github.com>
2026-03-16 18:48:39 -0500

    World-class design polish + critical accessibility fixes (contrast, focus states, reduced motion)

--- script.js
+++ script.js
@@ -116,8 +116,8 @@
         });
       },
       {
-        threshold: 0.15,
-        rootMargin: '0px 0px -40px 0px'
+        threshold: 0.1,
+        rootMargin: '0px 0px -60px 0px'
       }
     );
 
